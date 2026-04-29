e7af86b412c8f742210cac8358eadf88ca168adc
ZackCharlebois <zcharlebois01@gmail.com>
2026-04-29 13:00:10 -0400

    Improved particle physics and terrain effects when particles collide

--- Assets/Scripts/TerrainManager.cs
+++ Assets/Scripts/TerrainManager.cs
@@ -6,6 +6,9 @@ public class TerrainManager : MonoBehaviour
 {
     private HashSet<GameObject> terrainObjects;
 
+    [SerializeField] private Color waterColor = Color.blue;
+    [SerializeField] private float glowIntensity = 5.0f;
+
     void Awake()
     {
         terrainObjects = new HashSet<GameObject>();
@@ -33,10 +36,16 @@ public class TerrainManager : MonoBehaviour
 
         // Replace with whatever we want to happen when terrain is hit
         Renderer _renderer = hitObject.GetComponent<Renderer>();
+        Light _light = hitObject.GetComponent<Light>();
 
         if (_renderer != null)
         {
-            _renderer.material.color = Color.blue;
+            _renderer.material.color = waterColor;
+        }
+        if (_light != null)
+        {
+            _light.color = waterColor;
+            _light.intensity = glowIntensity;
         }
     }
 
